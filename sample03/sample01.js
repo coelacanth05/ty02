7a9d6371978418d1765418096e429f37786e66c3
kai <coelacanth2505@gmail.com>
2021-03-23 21:43:18 +0900

    52

---
+++ sample03/sample01.js
@@ -0,0 +1,5 @@
+function sum(a, b) {
+    return a * b;
+}
+var answer = sum(5, 2);
+console.log(answer);
---
+++ sample03/sample01.ts
@@ -0,0 +1,6 @@
+function sum(a,b) {
+  return a *b
+}
+
+const answer = sum(5, 2);
+console.log(answer);
